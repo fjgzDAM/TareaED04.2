4fe988ce46416354dccc56e61df0faf840492a2a
Francisco <fjgzarco@gmail.com>
2024-02-03 12:46:43 +0100

    Versión 2.1 texto

--- src/franciscojaviergarciazarco/git/FranciscoJavierGarciaZarcoGIT.java
+++ src/franciscojaviergarciazarco/git/FranciscoJavierGarciaZarcoGIT.java
@@ -15,7 +15,8 @@ public class FranciscoJavierGarciaZarcoGIT {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola Mundo!");
+        System.out.println("¡Hola Mundo!");
+        System.out.println("¿Todo bien?");
         
     }
     
